e6dff206e411186549bb0baf63e8f5e382dbc143
javigabbo <javigabbo@gmail.com>
2017-01-31 14:35:34 +0100

    Fichero sql

--- maquinaRefrescos.sql
+++ maquinaRefrescos.sql
@@ -1,78 +1,87 @@
--- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1    Database: maquinaRefrescos
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.16-MariaDB
+-- Host: localhost
+-- Generation Time: Jan 31, 2017 at 02:34 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maquinaRefrescos`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `depositos`
 --
 
-DROP TABLE IF EXISTS `depositos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `depositos` (
   `valor` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
-  `nombreMoneda` varchar(45) NOT NULL,
-  PRIMARY KEY (`valor`)
+  `nombreMoneda` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `depositos`
 --
 
-LOCK TABLES `depositos` WRITE;
-/*!40000 ALTER TABLE `depositos` DISABLE KEYS */;
-INSERT INTO `depositos` VALUES (0,1,''),(5,10,'Cinco centimos'),(10,9,'Diez centimos'),(20,9,'Veinte centimos'),(50,10,'Cincuenta centimos'),(100,11,'Un euro'),(200,10,'Dos euros');
-/*!40000 ALTER TABLE `depositos` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `depositos` (`valor`, `cantidad`, `nombreMoneda`) VALUES
+(10, 8, 'Diez cÃ©ntimos'),
+(20, 2, 'Veinte cÃ©ntimos'),
+(50, 3, 'Cincuenta centimos'),
+(100, 15, 'Un euro'),
+(200, 2, 'Dos euros');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `dispensadores`
 --
 
-DROP TABLE IF EXISTS `dispensadores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dispensadores` (
   `clave` varchar(45) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `precio` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  PRIMARY KEY (`clave`)
+  `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `dispensadores`
 --
 
-LOCK TABLES `dispensadores` WRITE;
-/*!40000 ALTER TABLE `dispensadores` DISABLE KEYS */;
-INSERT INTO `dispensadores` VALUES ('aguaGas','Agua con Gas',50,0),('aquarius','Aquarius',80,10),('coca','Coca-Cola',70,8),('fantaN','Fanta Naranja',60,9),('nestea','Nestea',100,10);
-/*!40000 ALTER TABLE `dispensadores` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `dispensadores` (`clave`, `nombre`, `precio`, `cantidad`) VALUES
+('caldo', 'Caldo', 50, 1),
+('coca', 'Coca', 100, 0),
+('fanta', 'Fanta de Naranja', 60, 3),
+('gazpacho', 'Pipas', 12, 4);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `depositos`
+--
+ALTER TABLE `depositos`
+  ADD PRIMARY KEY (`valor`);
+
+--
+-- Indexes for table `dispensadores`
+--
+ALTER TABLE `dispensadores`
+  ADD PRIMARY KEY (`clave`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-10-26 13:21:51
